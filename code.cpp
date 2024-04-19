c1af45f8c271c382b2f7aba729f53088408c8ddf
mczerrr <114849893+mczerrr@users.noreply.github.com>
2024-04-19 18:24:51 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -5,10 +5,10 @@ int main() {
     cout << "Content-type:text/html\r\n\r\n";
     cout << "<html>\n";
     cout << "<head>\n";
-    cout << "<title>Hello, World!</title>\n";
+    cout << "<title>Hello, World. Im Adarsh!</title>\n";
     cout << "</head>\n";
     cout << "<body>\n";
-    cout << "<h1>Hello, World!</h1>\n";
+    cout << "<h1>Hello, World. Adarsh is here!</h1>\n";
     cout << "</body>\n";
     cout << "</html>\n";
     return 0;
